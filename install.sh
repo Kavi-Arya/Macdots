2771dd7fa98f19b5ec3de085970804c73700f620
Kavi Arya <kvl@Kavis-MacBook-Air.local>
2023-08-03 10:34:09 +0530

    Adding install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+brewinstall(){
+  brew install keepassxc
+  brew install gimp
+  brew install qbittorrent
+  brew install onlyoffice
+  brew install obsidian
+  brew install obs
+  brew install mpv
+  brew install firefox
+  brew install flameshot
+  brew install alfred
+  brew install yt-dlp
+  brew install curl
+  brew install exa
+  brew install ffmpeg
+  brew install fzf
+  brew install harfbuzz
+  brew install imagemagick 
+  brew install lf
+  brew install lua
+  brew install neofetch
+  brew install newsboat
+  brew install pandoc
+  brew install python
+  brew install sketchybar
+  brew install skhd
+  brew install tmux
+  brew install tldr
+  brew install tree-sitter
+  brew install wget
+  brew install yabai
+  brew install kitty
+  brew install unnaturalscrollwheels
+  brew install visual-studio-code
+  brew install spaceman
+}
+
