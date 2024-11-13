5defbe4913fefb1ce23bf547b0bc605dd577281b
Kavi Arya <augest@Kavis-MacBook-Air.local>
2024-11-14 01:08:34 +0530

    updating neovim

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -28,8 +28,8 @@ vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definiti
 vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
 -- vim.cmd [[:amenu 10.120 mousemenu.-sep- *]]
 
-vim.keymap.set("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
-vim.keymap.set("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
+keymap("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
+keymap("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
 
 -- more good
 -- keymap({ "n", "o", "x" }, "<s-h>", "^", opts)
@@ -48,3 +48,36 @@ keymap({ "n" }, "<s-l>", "<cmd>tabn<cr>", opts)
 vim.api.nvim_set_keymap("t", "<C-;>", "<C-\\><C-n>", opts)
 
 keymap("n", "<leader>o", ":lua vim.opt.number = true; vim.opt.textwidth = 0; vim.opt.wrapmargin = 0; vim.opt.wrap = true; vim.opt.linebreak = true; vim.opt.columns = 120<CR>", opts)
+
+-- theprimegen remaps
+
+vim.g.mapleader = " "
+keymap("n", "<leader>pv", vim.cmd.Ex)
+
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+-- greatest remap ever
+keymap("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+keymap({"n", "v"}, "<leader>y", [["+y]])
+keymap("n", "<leader>Y", [["+Y]])
+
+keymap({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+keymap("i", "<C-c>", "<Esc>")
+
+keymap("n", "Q", "<nop>")
+keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap("n", "<leader>f", vim.lsp.buf.format)
+
+
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
---
+++ .config/nvim/lua/user/marvim.lua
@@ -0,0 +1,5 @@
+local M = {
+  "chamindra/marvim",
+}
+
+return M
