169629bc903a7ce32ea2f10c8d31fffba84c7eda
Kavi Arya <augest@Kavis-MacBook-Air.local>
2024-12-03 22:32:50 +0530

    Install.sh shit

--- src/install.sh
+++ src/install.sh
@@ -12,38 +12,8 @@ movingShit(){
 
 brewinstall(){
   echo '---------Brew Install---------'
-  brew install keepassxc
-  brew install qbittorrent
-  brew install onlyoffice
-  brew install obsidian
-  brew install mpv
-  brew install firefox
-  brew install flameshot
-  brew install alfred
-  brew install yt-dlp
-  brew install curl
-  brew install eza
-  brew install ffmpeg
-  brew install fzf
-  brew install harfbuzz
-  brew install imagemagick 
-  brew install lf
-  brew install lua
-  brew install neofetch
-  brew install newsboat
-  brew install pandoc
-  brew install python
-  brew install sketchybar
-  brew install skhd
-  brew install tmux
-  brew install tree-sitter
-  brew install wget
-  brew install yabai
-  brew install kitty
-  brew install unnaturalscrollwheels
-  brew install visual-studio-code
-  brew install spaceman
-  brew install iglace 
+  pkglist="./sorted.pkgs.txt"
+  brew install keepassxc qbittorrent onlyoffice obsidian mpv firefox flameshot alfred yt-dlp curl eza ffmpeg fzf harfbuzz imagemagick lf lua neofetch newsboat pandoc python sketchybar skhd tmux tree-sitter wget yabai kitty unnaturalscrollwheels visual-studio-code spaceman iglace 
   printf "\n"
 }
 
@@ -56,42 +26,43 @@ shellColor(){
 
 sysSettings(){
   echo '---------Changing macOS defaults...---------'
-  defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
-  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-  defaults write com.apple.spaces spans-displays -bool false
-  defaults write com.apple.dock autohide -bool true
-  defaults write com.apple.dock "mru-spaces" -bool "false"
-  defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-  defaults write com.apple.LaunchServices LSQuarantine -bool false
-  defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-  defaults write NSGlobalDomain KeyRepeat -int 1
-  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-  defaults write NSGlobalDomain _HIHideMenuBar -bool true
-  defaults write com.apple.screencapture location -string "$HOME/Desktop"
-  defaults write com.apple.screencapture disable-shadow -bool true
-  defaults write com.apple.screencapture type -string "png"
-  defaults write com.apple.finder DisableAllAnimations -bool true
-  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
-  defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
-  defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
-  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-  defaults write com.apple.Finder AppleShowAllFiles -bool true
-  defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-  defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-  defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-  defaults write com.apple.finder ShowStatusBar -bool false
-  defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
-  defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-  defaults write com.apple.Safari IncludeDevelopMenu -bool true
-  defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-  defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-  defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-  defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-  defaults write -globalDomain AppleAccentColor -bool False
-  defaults write -globalDomain AppleHighlightColor -string "Orange"
-  defaults write -globalDomain AppleAccentColor -bool True
+  defaultsWrite="defaults write"
+  $defaultsWrite com.apple.NetworkBrowser BrowseAllInterfaces 1
+  $defaultsWrite com.apple.desktopservices DSDontWriteNetworkStores -bool true
+  $defaultsWrite com.apple.spaces spans-displays -bool false
+  $defaultsWrite com.apple.dock autohide -bool true
+  $defaultsWrite com.apple.dock "mru-spaces" -bool "false"
+  $defaultsWrite NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+  $defaultsWrite com.apple.LaunchServices LSQuarantine -bool false
+  $defaultsWrite NSGlobalDomain com.apple.swipescrolldirection -bool false
+  $defaultsWrite NSGlobalDomain KeyRepeat -int 1
+  $defaultsWrite NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+  $defaultsWrite NSGlobalDomain AppleShowAllExtensions -bool true
+  $defaultsWrite NSGlobalDomain _HIHideMenuBar -bool true
+  $defaultsWrite com.apple.screencapture location -string "$HOME/Desktop"
+  $defaultsWrite com.apple.screencapture disable-shadow -bool true
+  $defaultsWrite com.apple.screencapture type -string "png"
+  $defaultsWrite com.apple.finder DisableAllAnimations -bool true
+  $defaultsWrite com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+  $defaultsWrite com.apple.finder ShowHardDrivesOnDesktop -bool false
+  $defaultsWrite com.apple.finder ShowMountedServersOnDesktop -bool false
+  $defaultsWrite com.apple.finder ShowRemovableMediaOnDesktop -bool false
+  $defaultsWrite com.apple.Finder AppleShowAllFiles -bool true
+  $defaultsWrite com.apple.finder FXDefaultSearchScope -string "SCcf"
+  $defaultsWrite com.apple.finder FXEnableExtensionChangeWarning -bool false
+  $defaultsWrite com.apple.finder _FXShowPosixPathInTitle -bool true
+  $defaultsWrite com.apple.finder FXPreferredViewStyle -string "Nlsv"
+  $defaultsWrite com.apple.finder ShowStatusBar -bool false
+  $defaultsWrite com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
+  $defaultsWrite com.apple.Safari AutoOpenSafeDownloads -bool false
+  $defaultsWrite com.apple.Safari IncludeDevelopMenu -bool true
+  $defaultsWrite com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+  $defaultsWrite com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+  $defaultsWrite NSGlobalDomain WebKitDeveloperExtras -bool true
+  $defaultsWrite com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+  $defaultsWrite -globalDomain AppleAccentColor -bool False
+  $defaultsWrite -globalDomain AppleHighlightColor -string "Orange"
+  $defaultsWrite -globalDomain AppleAccentColor -bool True
 }
 
 Services(){
