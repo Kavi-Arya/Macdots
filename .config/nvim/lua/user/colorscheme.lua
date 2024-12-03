ebe3aff40e53e7872fda2cf663c495b361c275f3
Kavi Arya <augest@Kavis-MacBook-Air.local>
2024-12-03 23:58:33 +0530

    .

---
+++ .config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,17 @@
+local M = {
+  -- "LunarVim/darkplus.nvim",
+  -- "Mofiqul/vscode.nvim",
+  -- "sainnhe/gruvbox-material",
+  -- "shaunsingh/nord.nvim",
+  -- "slugbyte/lackluster.nvim",
+  -- "sainnhe/everforest",
+  -- "catppuccin/nvim",
+  -- "cryptomilk/nightcity.nvim",
+  "rose-pine/neovim",
+}
+
+function M.config()
+  vim.cmd.colorscheme "rose-pine"
+end
+
+return M
