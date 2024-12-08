439185ceb045378784fc2205de0eebfa337d6123
Kavi Arya <kaviarya047@tutamail.com>
2024-12-08 19:45:23 +0530

    .

--- .config/sketchybar/plugins/nowPlaying.sh
+++ .config/sketchybar/plugins/nowPlaying.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 nowPlaying=$(nowplaying-cli get title artist | gpaste -sd '-' | sed 's/-/ - /')
-sketchybar --set "$NAME" icon=" " label="Now Playing: '$nowPlaying'"
+sketchybar --set "$NAME" icon=" " label="Now Playing: $nowPlaying"
